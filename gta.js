652b2398a8bf9a338feb7ee40edcabcf9744dfed
Priyanshu100704 <thakurpriyan6328@gmail.com>
2024-09-07 10:13:12 +0530

    Add files via upload

---
+++ gta.js
@@ -0,0 +1,9 @@
+let btn1=document.querySelector("#btn1");
+btn1.addEventListener("click",()=>{
+    btn1.style.backgroundColor="red";
+    btn1.style.color="white";
+});
+let btnA=document.querySelector(".btnA");
+btnA.addEventListener("mouseover",()=>{
+    btnA.style.fontWeight="bolder";
+});
